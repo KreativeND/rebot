cdebdfeccbdd3fc08c87d834e386df9fe230932b
KreativeND <155289206+KreativeND@users.noreply.github.com>
2024-02-24 09:23:35 +0000

    added checkbox

--- webview-ui/src/components/customComponents/refactorPanel.tsx
+++ webview-ui/src/components/customComponents/refactorPanel.tsx
@@ -3,6 +3,7 @@ import { SheetContent, SheetDescription, SheetHeader, SheetTitle } from '@/compo
 import './panel.css';
 import { Card, CardContent } from '@/components/ui/card';
 import { CiFileOn } from "react-icons/ci";
+import { CheckboxReactHookFormMultiple } from './refactorGuildLines';
 
 const RefactorPanel = ({ fileNodes }) => {
     return (
@@ -14,7 +15,7 @@ const RefactorPanel = ({ fileNodes }) => {
                         <CardContent className='w-full p-4 h-[300px] overflow-auto scroll_area'>
                             {
                                 fileNodes.map((fileNode, index) => (
-                                    <div key={index} className='flex flex-row items-start space-x-3 space-y-0 rounded-md border p-4 shadow'>
+                                    <div key={index} className='flex flex-row items-start space-y-1 rounded-md border p-4 shadow'>
                                         <div className='flex h-full items-center'>
                                             <CiFileOn size={"40px"} />
                                             <div className='flex flex-col justify-center'>
@@ -32,7 +33,7 @@ const RefactorPanel = ({ fileNodes }) => {
                         </CardContent>
                     </Card>
                     <SheetTitle>Refactoring rules:</SheetTitle>
-
+                    <CheckboxReactHookFormMultiple />;
                 </SheetHeader>
             </SheetContent>
         </div>
